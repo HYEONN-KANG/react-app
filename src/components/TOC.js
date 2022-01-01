a3a2293445e29b12ebc5b20f5f00b001f9bc8e13
kang hyeon <hyeonkang.dev@gmail.com>
2022-01-02 01:31:59 +0900

    create, update, delete

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
 
 class TOC extends Component {
+  shouldComponentUpdate(newProps, newState) {
+    console.log(
+      "===> TOC render shouldComponentUpdate",
+      newProps,
+      this.props.data
+    );
+    // newProps 는 바뀐 새로운 props, this.props는 기존의 props
+    // App.js 의 else if (this.state.mode === "create") 에서 contents 변경 시
+    // cpush를 사용하여 직접 데이터를 변경할 경우 newProps 와 this.props는 동일하다.
+    // 따라서 데이터 변경 시 원본을 직접 바꾸지 않는 것을 권장한다. concat 이나 Arra.from 등 권장
+
+    // this.props.data에 변경이 있을경우에만 render 함수 호출
+    if (this.props.data === newProps.data) {
+      return false;
+    }
+    return true;
+  }
   render() {
+    console.log("===> TOC render ");
     var data = this.props.data;
     var lists = [];
     var i = 0;
